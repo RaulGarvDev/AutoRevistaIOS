7cb12372f0786411f872ba502c3c5482e15e589f
Raúl González <raulgonzalezdelaaleja@gmail.com>
2021-12-23 11:56:29 +0100

    Initial Commit

---
+++ AutoRevista/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoRevista
+//
+//  Created by raul gonzalez de la aleja ruiz valdepeñas on 23/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
